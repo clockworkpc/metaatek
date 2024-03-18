4fbaa7288f746739dd4932b4ca8eab5fca18ef06
Alexander Garber <clockworkpc@gmail.com>
2024-03-18 09:57:39 +0200

    Diff check (#2)
    * local changes
    
    * still not working css
    
    * copied all files
    
    * rubocop
    
    * some diff changes
    
    * working now
    
    * extra changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,8 +8,11 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap/dist/js')
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
 
+Rails.application.config.assets.paths << Rails.root.join('app/assets/fonts')
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[*.svg *.eot *.woff *.woff2 *.ttf]
 Rails.application.config.assets.precompile += %w[bootstrap.min.js bootstrap.rtl.min.js custom_fonts.css]
